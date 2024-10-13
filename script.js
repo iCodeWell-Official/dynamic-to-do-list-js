8d80aef5ca42fbe51bd4d70f8c5a90f25e08475b
Nasr <nasr.asri10@gmail.com>
2024-10-13 18:44:01 +0100

    attach event listeners

---
+++ script.js
@@ -0,0 +1,44 @@
+// Setup Event Listener for Page Load:
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Select DOM Elements:
+    
+    const addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+
+    // Create the addTask Function:
+
+    function addTask () {
+        const taskText = taskInput.value.trim();
+        if (taskText === '') {
+            alert('Please enter a task first!');
+        } else {
+
+            // Task Creation and Removal:
+
+            const listItem = document.createElement('li');
+            listItem.textContent = taskText;
+            const removeBtn = document.createElement('button');
+            removeBtn.textContent = 'Remove';
+            removeBtn.classList.add('remove-btn');
+            removeBtn.addEventListener('onclick', () => {
+                listItem.remove();
+            })
+            listItem.appendChild(removeBtn);
+            taskList.appendChild(listItem);
+            taskInput.value = '';
+        }
+    }
+
+    // Attach Event Listeners:
+
+    addButton.addEventListener('click', () => {
+        addTask ();
+    })
+    taskInput.addEventListener('keypress', function (event) {
+        if (event.key === 'enter') {
+            addTask ();
+        }
+    })
+})
